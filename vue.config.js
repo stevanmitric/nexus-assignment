07517b585f7e320bea8af9e9d0302a02b3cf413a
Stevan <mitricstevan5@gmail.com>
2024-11-15 17:27:09 +0100

    deployment

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/nexus-assignment',
   configureWebpack: {
     plugins: [
       new (require('webpack').DefinePlugin)({
